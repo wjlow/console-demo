f6c884a87d745b7a6aa6d0494e39926df1189297
Jack Low <jlow@zendesk.com>
2020-04-06 14:59:53 +1000

    Latest version

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,57 +1,61 @@
 import scala.io.StdIn
 
-/**
- * Practice code from http://degoes.net/articles/easy-monads
- */
-
-sealed trait Console[A] {
-  def map[B](transformer: A => B): Console[B] = Map(this, transformer)
-
-  def flatMap[B](next: A => Console[B]): Console[B] = Chain(this, next)
-}
-
-case class WriteLine(line: String) extends Console[Unit]
-
-case object ReadLine extends Console[String]
-
-case class Map[Z, A](console: Console[Z], transformer: Z => A) extends Console[A]
-
-case class Chain[Z, A](console: Console[Z], next: Z => Console[A]) extends Console[A]
-
-case class Pure[A](value: A) extends Console[A]
-
 object Main {
 
-  def main(args: Array[String]): Unit = {
-    val program =
-      WriteLine("Type something!").flatMap(_ =>
-        ReadLine
-          .flatMap(userInput =>
-            WriteLine(s"The user typed: $userInput")
-              .map(_ => userInput)))
+  // Console program
+  // Writes a string
+  // Reads user input
 
-    val lengthProgram: Console[Int] = program.map((str: String) => str.length)
-
-    val result = interpret(lengthProgram)
-
-    println(s"The length of the message is: $result")
-
-    ()
+  // Console[A]: when interpreted, it will give you a VALUE of TYPE A
+  // interpret(Console[Int]) ----> 1 or 2 or 3 etc.
+  sealed trait Console[A] {
+    def map[B](f: A => B): Console[B] = Map(this, f)
+    def flatMap[B](f: A => Console[B]): Console[B] = FlatMap(this, f)
   }
 
+  case class WriteLine(line: String) extends Console[Unit]
+
+  case object ReadLine extends Console[String]
+
+  case class Map  [Z, A](consoleZ: Console[Z], f: Z => A) extends Console[A]
+
+  case class FlatMap[Z, A](consoleZ: Console[Z], f: Z => Console[A]) extends Console[A]
+
+  case class EndWith[A](result: A) extends Console[A]
+
+  def main(args: Array[String]): Unit = {
+
+    val program = ReadLine.flatMap((name: String) => WriteLine(s"Hello $name").map(_ => name))
+
+    val anotherProgram: Console[Int] = program.map((name: String) => name.length)
+
+    val program3 = anotherProgram.flatMap((num: Int) => WriteLine(s"The length is $num"))
+
+    val result = interpret(program3)
+
+    println("\n***\n")
+
+    println(s"Result is: $result")
+
+    ()
+
+  }
+
+  // Console[A]: when interpreted, it will give you a VALUE of TYPE A
   def interpret[A](console: Console[A]): A =
     console match {
       case WriteLine(line) => println(line)
       case ReadLine => StdIn.readLine()
-      case Map(consoleZ, transformer) =>
+      case Map(consoleZ, f) =>
         val z = interpret(consoleZ)
-        val a = transformer(z)
+        val a = f(z)
         a
-      case Chain(consoleZ, next) =>
+      case FlatMap(consoleZ, f) =>
         val z = interpret(consoleZ)
-        val consoleA = next(z)
-        interpret(consoleA)
-      case Pure(value) => value
+        val consoleA = f(z)
+        val a = interpret(consoleA)
+        a
+      case EndWith(result) => result
     }
 
 }
